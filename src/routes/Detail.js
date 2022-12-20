e7e8a91456b8c3e5d463accc668993c7a022a219
thjang93 <thjang93@gmail.com>
2022-12-20 20:03:39 +0900

    add detail styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './Detail.module.css';
 
+
 function Detail() {
     const { id } = useParams();
     const [loading, setLoading] = useState(true);
@@ -16,6 +17,7 @@ function Detail() {
         ).json();
         setMovie(json.data.movie);
         setLoading(false);
+        console.log(movie);
     }
 
     useEffect(() => {
@@ -23,16 +25,28 @@ function Detail() {
     }, []);
 
     return (
-        <div>
+        <div className={styles.container}>
             {loading ?
-                (<h1>Loading...</h1>) :
-                (<div>
-                    <img src={movie.medium_cover_image} alt={movie.title}/>
-                    <h2>{movie.title}</h2>
-                    <p>
-                        {movie.description_full}
-                    </p>
-                </div>)
+                (
+                    <h1>Loading...</h1>
+                ) :
+                (
+                    <div className={styles.detail}>
+                        <img className={styles.detail__image} src={movie.medium_cover_image} alt={movie.title}/>
+                        <div>
+                            <h2>{movie.title}</h2>
+                            <h3>{movie.year}</h3>
+                            <h3>rate : {movie.rating.toFixed(1)}</h3>
+                            <h3>runtime : {movie.runtime ? movie.runtime : "unknown"}</h3>
+                            <p>{movie.description_full}</p>
+                            <ul className={styles.detail__genres}>
+                                {movie.genres.map(genre => (
+                                    <li key={genre}>{genre}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                )
             }
         </div>
     );
